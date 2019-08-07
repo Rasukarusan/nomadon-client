ec025fc1689d6ab7b18998805a1baf046189c334
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-08-07 12:46:08 +0900

    Initial Commit

---
+++ nomadon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nomadon
+//
+//  Created by tanaka naoto on 2019/08/07.
+//  Copyright © 2019 tanaka.naoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
